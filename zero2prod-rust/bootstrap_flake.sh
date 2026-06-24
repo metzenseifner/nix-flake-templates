f31c60e1ac2dbfdabd6ee72f7ae0e910dda6b868
Jonathan L. Komar <komar.jonathan@gmail.com>
2026-06-24 13:45:56 +0200

    feat: add bootstrap_flake.sh script since steps are prereqs for the helpful shell hook

---
+++ zero2prod-rust/bootstrap_flake.sh
@@ -0,0 +1,11 @@
+# Day-0 Steps (Ensure the following)
+
+#  Cargo.lock committed
+cargo generate-lockfile; git add Cargo.lock
+# deny.toml committed; else cargo deny init, then commit
+
+cargo deny init; git add deny.toml
+
+# .gitignore contains result and result-* as Nix will output build artifacts there.
+
+nix flake check --all-systems # check aforementioned  (or e.g. check for deny.toml by running nix build .#checks.<sys>.deny)
